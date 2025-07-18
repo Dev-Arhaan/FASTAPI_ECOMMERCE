f8ec99a3c81a1c3883ec521998fe8a2c32b9e77f
dev-arhaan <dev.sayarhaan@gmail.com>
2025-07-19 03:56:29 +0530

    Revert "Add existing project files to Git"
    This reverts commit d9062a53606629830695ed8188da2a5f2ce1f7a3.
    ok

--- app/utils/pagination.py
+++
@@ -1,9 +0,0 @@
-def paginate(data: list, offset: int, limit: int) -> dict:
-    return {
-        "data": data,
-        "page": {
-            "next": str(offset + limit),
-            "limit": limit,
-            "previous": str(offset - limit)
-        }
-    }
